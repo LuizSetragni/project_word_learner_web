a5caaaec7c23cc4148e412c7014529c01170e7c6
Luiz Setragni <luiz.lelis_accenture@prestador.globo>
2024-05-04 18:48:01 -0300

    add new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { UserRegisterComponent } from './components/user-register/user-register.component';
 import { NewUserComponent } from './components/new-user/new-user.component';
+import { LoginWordLearnerComponent } from './components/login-word-learner/login-word-learner.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'about', component: AboutComponent},
     {path: 'user-register', component: UserRegisterComponent},
-    {path: 'login', component: NewUserComponent}
+    {path: 'new-user', component: NewUserComponent},
+    {path: 'login-word-learner', component: LoginWordLearnerComponent}
 ];
