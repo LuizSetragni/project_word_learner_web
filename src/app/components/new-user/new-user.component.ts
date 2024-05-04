77b6dae96372ce500547a300f5138005a4279b7e
Luiz Setragni <luiz.lelis_accenture@prestador.globo>
2024-05-04 18:47:08 -0300

    refactor component newUser

--- src/app/components/new-user/new-user.component.ts
+++ src/app/components/new-user/new-user.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-new-user',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './new-user.component.html',
   styleUrl: './new-user.component.css'
 })
 export class NewUserComponent {
-email: any;
 
+
+  email: any;
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+
+  }
+
+  public emailExist(email:string){
+    if(false)//buscar api no banco, se existir, passar email como paramentro, input já preenchido falndo colocar senha 
+    this.router.navigate(['/login-word-learner'])
+    else{//vai fazer registro completo com email já preenchido passando por parametro
+      
+      this.router.navigate(['/user-register'])
+    }
+
+  }
 }
