cd02dbd06224f5aee205c50adafb36909c9282dd
Luiz Setragni <luiz.lelis_accenture@prestador.globo>
2024-05-04 01:01:58 -0300

    config routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { UserRegisterComponent } from './components/user-register/user-register.component';
+import { NewUserComponent } from './components/new-user/new-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'user-register', component: UserRegisterComponent},
+    {path: 'login', component: NewUserComponent}
+];
