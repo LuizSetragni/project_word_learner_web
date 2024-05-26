c03ee12ed2f94d39494c3e29f8077179af62bade
Luiz Setragni <luiz.lelis_accenture@prestador.globo>
2024-05-26 20:20:21 -0300

    fix file user

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -7,11 +7,15 @@ import { Observable } from 'rxjs';
 })
 export class UserService {
 
-  API_URL: string = "http://127.0.0.1:8000/api/";
+  private API_URL: string = "http://127.0.0.1:8000/api/";
 
   constructor(private httpClient: HttpClient) { }
 
   registerUser(userData: any): Observable<any> {
     return this.httpClient.post(`${this.API_URL}register/`, userData);
+  }
+
+  login(credentials: { username: string, password: string }): Observable<any> {
+    return this.httpClient.post(`${this.API_URL}/token/`, credentials);
   }
 }
