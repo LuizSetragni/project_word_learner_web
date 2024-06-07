309e2eeddcd2ea46ff0e7f1400aa24c4567f0a3b
Luiz Setragni <luiz.lelis_accenture@prestador.globo>
2024-06-07 01:50:53 -0300

    new about

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,18 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { JwtUtils } from '../../utils/jwtUtils';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  isLogado: boolean = false;
+  private token: any;
+
   constructor(private router: Router) { }
 
-  goToLogin() {
-    this.router.navigate(['/login-word-learner']);
+  ngOnInit(): void {
+    if (typeof localStorage !== 'undefined') {
+      this.token = localStorage.getItem('token');
+      if (this.token) JwtUtils.decodeJwt(this.token);
+      this.isLogado = JwtUtils.isLogado;
+    } else {
+      console.warn('localStorage is not available');
+    }
   }
 
+  openRouter(route: string): void {
+    this.router.navigate([`${route}`]);
+  }
 }
